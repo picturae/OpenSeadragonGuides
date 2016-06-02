595c8eba4a18da46ccbf7b20097b67962b941cc7
Hendrik Lammers <h.lammers@picturae.nl>
2016-06-02 15:26:27 +0200

    Change structure

--- src/index.js
+++ src/index.js
@@ -1,15 +1,102 @@
-import './plugin';
+import {Guide} from './guide';
+import {$, DIRECTION_HORIZONTAL, DIRECTION_VERTICAL} from './globals';
 
-// Demo viewer
-const viewer = OpenSeadragon({
-  id: 'viewer-container',
-  prefixUrl: 'node_modules/openseadragon/build/openseadragon/images/',
-  crossOriginPolicy: 'Anonymous',
-  defaultZoomLevel: 1,
-  tileSources: 'http://openseadragon.github.io/example-images/highsmith/highsmith.dzi',
-  minZoomImageRatio: 0.1
+if (!$.version || $.version.major < 2) {
+  throw new Error(
+    'This version of OpenSeadragon Guides requires OpenSeadragon version 2.0.0+'
+  );
+}
+
+$.Viewer.prototype.guides = function(options) {
+  if (!this.guidesInstance || options) {
+    options = options || {};
+    options.viewer = this;
+    this.guidesInstance = new $.Guides(options);
+  }
+
+  return this.guidesInstance;
+};
+
+$.Guides = function(options) {
+  $.extend(true, this, {
+    // internal state properties
+    viewer: null,
+    guides: [],
+    guideIndex: 0,
+
+    // options
+    horizontalGuideButton: null,
+    verticalGuideButton: null,
+    prefixUrl: null,
+    navImages: {
+      guideHorizontal: {
+        REST: 'button_rest.png',
+        GROUP: 'button_grouphover.png',
+        HOVER: 'button_hover.png',
+        DOWN: 'button_pressed.png'
+      },
+      guideVertical: {
+        REST: 'button_rest.png',
+        GROUP: 'button_grouphover.png',
+        HOVER: 'button_hover.png',
+        DOWN: 'button_pressed.png'
+      }
+    }
+  }, options);
+
+  $.extend(true, this.navImages, this.viewer.navImages);
+
+  const prefix = this.prefixUrl || this.viewer.prefixUrl || '';
+  const useGroup = this.viewer.buttons && this.viewer.buttons.buttons;
+  const anyButton = useGroup ? this.viewer.buttons.buttons[0] : null;
+  const onFocus = anyButton ? anyButton.onFocus : null;
+  const onBlur = anyButton ? anyButton.onBlur : null;
+
+  this.horizontalGuideButton = new $.Button({
+    element: this.horizontalGuideButton ? $.getElement(this.horizontalGuideButton) : null,
+    clickTimeThreshold: this.viewer.clickTimeThreshold,
+    clickDistThreshold: this.viewer.clickDistThreshold,
+    tooltip: $.getString('Tooltips.HorizontalGuide') || 'Horizontal guide',
+    srcRest: prefix + this.navImages.guideHorizontal.REST,
+    srcGroup: prefix + this.navImages.guideHorizontal.GROUP,
+    srcHover: prefix + this.navImages.guideHorizontal.HOVER,
+    srcDown: prefix + this.navImages.guideHorizontal.DOWN,
+    onRelease: this.createHorizontalGuide.bind(this),
+    onFocus,
+    onBlur
+  });
+
+  this.verticalGuideButton = new $.Button({
+    element: this.verticalGuideButton ? $.getElement(this.verticalGuideButton) : null,
+    clickTimeThreshold: this.viewer.clickTimeThreshold,
+    clickDistThreshold: this.viewer.clickDistThreshold,
+    tooltip: $.getString('Tooltips.VerticalGuide') || 'vertical guide',
+    srcRest: prefix + this.navImages.guideVertical.REST,
+    srcGroup: prefix + this.navImages.guideVertical.GROUP,
+    srcHover: prefix + this.navImages.guideVertical.HOVER,
+    srcDown: prefix + this.navImages.guideVertical.DOWN,
+    onRelease: this.createVerticalGuide.bind(this),
+    onFocus,
+    onBlur
+  });
+
+  if (useGroup) {
+    this.viewer.buttons.buttons.push(this.horizontalGuideButton);
+    this.viewer.buttons.element.appendChild(this.horizontalGuideButton.element);
+    this.viewer.buttons.buttons.push(this.verticalGuideButton);
+    this.viewer.buttons.element.appendChild(this.verticalGuideButton.element);
+  }
+};
+
+$.extend($.Guides.prototype, {
+  createHorizontalGuide() {
+    const id = this.guideIndex++;
+    this.guides.push(new Guide(this.viewer, id, DIRECTION_HORIZONTAL));
+  },
+
+  createVerticalGuide() {
+    const id = this.guideIndex++;
+    this.guides.push(new Guide(this.viewer, id, DIRECTION_VERTICAL));
+  }
 });
 
-viewer.guides({
-  prefixUrl: '/images/'
-});
